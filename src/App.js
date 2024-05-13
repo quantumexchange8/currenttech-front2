6d62290b045833ec66cfdede0a43d297a4f7cc40
darren <darren.kooyz@gmail.com>
2024-05-13 13:27:04 +0800

    fix: wording

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Topbar from './Pages/Topbar';
 import Footer from './Pages/Footer';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Advantage from "./Pages/Advantage";
 import Products from "./Pages/Products";
